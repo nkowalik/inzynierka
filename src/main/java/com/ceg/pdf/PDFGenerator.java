0b651a12e45042a95b8272214f5c2ab8cbe274a5
Corson <pawellal@gmail.com>
2016-06-26 10:46:13 +0200

    .

--- src/main/java/com/ceg/pdf/PDFGenerator.java
+++ src/main/java/com/ceg/pdf/PDFGenerator.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import static javax.imageio.ImageIO.getCacheDirectory;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
