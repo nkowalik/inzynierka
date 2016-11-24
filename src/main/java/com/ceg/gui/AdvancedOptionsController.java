db2264107f01168688f607b6a388526334f481a9
Corson <pawellal@gmail.com>
2016-11-24 19:27:09 +0100

    Usuniecie zbednych importow

--- src/main/java/com/ceg/gui/AdvancedOptionsController.java
+++ src/main/java/com/ceg/gui/AdvancedOptionsController.java
@@ -4,7 +4,6 @@ import com.ceg.examContent.Exam;
 import com.ceg.pdf.PDFSettings;
 import com.ceg.utils.FontTypeUtil;
 
-import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -14,13 +13,11 @@ import java.util.ResourceBundle;
 
 import com.ceg.xml.TaskData;
 import com.ceg.xml.TasksLoading;
-import com.google.common.collect.ArrayTable;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
